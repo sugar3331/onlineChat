3d5891a124e3225b8a94d7ff72fa02a47e8726b4
sugar <sugar363@outlook.com>
2023-09-02 14:33:32 +0800

    firstcommit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	im "OnlineChat/handler"
+	"OnlineChat/tools"
+	"log"
+	"net/http"
+)
+
+func main() {
+	//chatRoom := im.NewChatRoom()
+	http.HandleFunc("/publicChat", tools.Token.ImJwtAuthMiddleware(im.HandIeWebSocket))
+	http.HandleFunc("/p2pChat", tools.Token.ImJwtAuthMiddleware(im.HandleP2PChat))
+	log.Println("IM Websocket Starting server at: 9090...")
+	err := http.ListenAndServe(":9090", nil)
+	if err != nil {
+		log.Fatal("聊天服务启动失败！！！:", err)
+	}
+}
